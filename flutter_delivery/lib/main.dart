981a79972813cd280295b58cf5f5d7a94c232a0e
Erick Avendaño <erick.avendano@igrtec.com>
2022-03-17 16:58:19 -0600

    Inicio de curso y proyecto

---
+++ flutter_delivery/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App Flutter',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {'login': (BuildContext context) => LoginPage()},
+    );
+  }
+}
