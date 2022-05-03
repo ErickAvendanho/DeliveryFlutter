7ffe1d558860aae56909f81252da016f6f8c6f0c
Erick Avendaño <erick.avendano@igrtec.com>
2022-05-03 17:23:04 -0500

    Cierre de sesión

--- flutter_delivery/lib/src/pages/client/products/list/client_products_list_page.dart
+++ flutter_delivery/lib/src/pages/client/products/list/client_products_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_delivery/src/pages/client/products/list/clients_products_list_controller.dart';
 
 class ClientProductsListPage extends StatefulWidget {
   const ClientProductsListPage({Key key}) : super(key: key);
@@ -8,11 +10,27 @@ class ClientProductsListPage extends StatefulWidget {
 }
 
 class _ClientProductsListPageState extends State<ClientProductsListPage> {
+  ClientProductsListController _con = new ClientProductsListController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      _con.init(context);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('LISTA DE PRODUCTOS'),
+        child: ElevatedButton(
+          onPressed: () {
+            _con.logout();
+          },
+          child: Text('Cerrar sesion'),
+        ),
       ),
     );
   }
