68337e28a70425a25aca527cf987213cddba9a6f
Erick Avendaño <erick.avendano@igrtec.com>
2022-05-03 16:12:10 -0500

    Login desde aplicación

--- flutter_delivery/lib/src/pages/login/login_controller.dart
+++ flutter_delivery/lib/src/pages/login/login_controller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/models/response_api.dart';
+import 'package:flutter_delivery/src/provider/users_provider.dart';
+import 'package:flutter_delivery/src/utils/my_snackbar.dart';
 
 class LoginController {
   BuildContext context;
@@ -6,19 +9,23 @@ class LoginController {
   TextEditingController emailController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
 
-  Future init(BuildContext context) {
+  UsersProvider usersProvider = new UsersProvider();
+
+  Future init(BuildContext context) async {
     this.context = context;
+    await usersProvider.init(context);
   }
 
   void goToRegisterPage() {
     Navigator.pushNamed(context, 'register');
   }
 
-  void login() {
+  void login() async {
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
-
-    print('EMAIL: $email');
-    print('PASSWORD: $password');
+    ResponseApi responseApi = await usersProvider.login(email, password);
+    print('Respuesta object: ${responseApi}');
+    print('Respuesta: ${responseApi.toJson()}');
+    MySnackBar.show(context, responseApi.message);
   }
 }
