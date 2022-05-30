473e2c35412693fd73667dc2067e47c3f42b0c8f
AlanRoberto19 <alan.mondragon@igrtec.com>
2022-05-30 09:15:46 -0500

    Update restaurant_categories_create_page.dart

--- flutter_delivery/lib/src/pages/restaurant/categories/create/restaurant_categories_create_page.dart
+++ flutter_delivery/lib/src/pages/restaurant/categories/create/restaurant_categories_create_page.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_delivery/src/pages/restaurant/categories/create/restaurant_categories_create_controller.dart';
-import 'package:flutter_delivery/src/utils/my_colors.dart';
+import '../../../../utils/my_colors.dart';
 
 class RestaurantCategoriesCreatePage extends StatefulWidget {
-  const RestaurantCategoriesCreatePage({Key key}) : super(key: key);
+  const RestaurantCategoriesCreatePage({Key key }) : super(key: key);
 
   @override
-  _RestaurantCategoriesCreatePageState createState() =>
-      _RestaurantCategoriesCreatePageState();
+  State<RestaurantCategoriesCreatePage> createState() => _RestaurantCategoriesCreatePageState();
 }
 
-class _RestaurantCategoriesCreatePageState
-    extends State<RestaurantCategoriesCreatePage> {
-  RestaurantCategoriesCreateController _con =
-      new RestaurantCategoriesCreateController();
+class _RestaurantCategoriesCreatePageState extends State<RestaurantCategoriesCreatePage> {
+
+  RestaurantCategoriesCreateController _con = new RestaurantCategoriesCreateController();
 
   @override
   void initState() {
@@ -28,81 +26,76 @@ class _RestaurantCategoriesCreatePageState
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nueva categoría'),
-        backgroundColor: MyColors.primaryColor,
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 30),
-          _textFieldName(),
-          _textFieldDescription(),
+       title: Text('Nueva Categoria'),
+       ),
+       body: Column(children: [
+         SizedBox(height: 30),
+         _textFieldName(),
+         _textFieldDesciption()
         ],
-      ),
-      bottomNavigationBar: _buttonCreate(),
+       ),
+      bottomNavigationBar: _buttonCreate(), 
     );
   }
 
-  Widget _textFieldName() {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 5),
-      decoration: BoxDecoration(
-        color: MyColors.primaryOpacityColor,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: TextField(
-        controller: _con.nameController,
-        decoration: InputDecoration(
-          hintText: 'Nombre de la categoria',
-          border: InputBorder.none,
-          contentPadding: const EdgeInsets.all(15),
-          hintStyle: TextStyle(color: MyColors.primaryColorDark),
-          suffixIcon: Icon(
-            Icons.list_alt,
-            color: MyColors.primaryColor,
+     Widget _textFieldDesciption() {
+      return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: TextField(
+          controller: _con.descriptionController,
+          maxLines: 3,
+          maxLength: 255,
+          decoration: InputDecoration(
+            hintText: 'Descripcion de la categoria',
+            border: InputBorder.none,
+            contentPadding: const EdgeInsets.all(15),
+            hintStyle: TextStyle(
+              color: MyColors.primaryColorDark),
+            suffixIcon: Icon(
+              Icons.description,
+              color: MyColors.primaryColor,
+            ),
           ),
         ),
-        cursorColor: MyColors.primaryColor,
-        textInputAction: TextInputAction.next,
-      ),
-    );
-  }
+      );
+    }
 
-  Widget _textFieldDescription() {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 5),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: MyColors.primaryOpacityColor,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: TextField(
-        controller: _con.descriptionController,
-        maxLines: 3,
-        maxLength: 255,
-        decoration: InputDecoration(
-          hintText: 'Descripcion de la categoria',
-          border: InputBorder.none,
-          contentPadding: const EdgeInsets.all(15),
-          hintStyle: TextStyle(color: MyColors.primaryColorDark),
-          suffixIcon: Icon(
-            Icons.description,
-            color: MyColors.primaryColor,
+    Widget _textFieldName() {
+      return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+        decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: TextField(
+          controller: _con.nameController,
+          decoration: InputDecoration(
+            hintText: 'Nombre de la categoria',
+            border: InputBorder.none,
+            contentPadding: const EdgeInsets.all(15),
+            hintStyle: TextStyle(color: MyColors.primaryColorDark),
+            suffixIcon: Icon(
+              Icons.list_alt,
+              color: MyColors.primaryColor,
+            ),
           ),
         ),
-        cursorColor: MyColors.primaryColor,
-        textInputAction: TextInputAction.next,
-      ),
-    );
-  }
+      );
+    }
 
-  Widget _buttonCreate() {
+    Widget _buttonCreate() {
     return Container(
       height: 50,
       width: double.infinity,
       margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
       child: ElevatedButton(
         onPressed: _con.createCategory,
-        child: const Text('Crear categoria'),
+        child: const Text('Crear Categoria'),
         style: ElevatedButton.styleFrom(
             primary: MyColors.primaryColor,
             shape:
@@ -115,4 +108,5 @@ class _RestaurantCategoriesCreatePageState
   void refresh() {
     setState(() {});
   }
+
 }
